75d7e34bd3d384bb2769515b00cae5ee8cc601c3
kbpsystem <koleen.bp@outlook.com>
2020-03-17 01:07:09 +0800

    adding Nav from reactstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="nav-fixed-top">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarColor02"
-          aria-controls="navbarColor02"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarColor02">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+        <NavbarBrand href="/">🦠 COVID19 Tracker</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/news">
+                  News
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://www.koleenbp.com">
+                  KBPsystem
+                </a>
+              </li>
+            </ul>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
