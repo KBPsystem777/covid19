4da582f29265b8f7e2ea5293c9c96340bf9701b7
kbpsystem <koleen.bp@outlook.com>
2020-04-04 00:13:14 +0800

    fix app name

--- public/worker.js
+++ public/worker.js
@@ -27,7 +27,7 @@ self.addEventListener("fetch", event => {
 
 // Update a service worker
 self.addEventListener("activate", event => {
-  let cacheWhitelist = ["your-app-name"];
+  let cacheWhitelist = ["COVID2019"];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
