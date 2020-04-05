bd944cbf757587c95e92a368114e0e90450f2b98
kbpsystem <koleen.bp@outlook.com>
2020-04-06 02:49:56 +0800

    added card and pointed to new api

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import NumberFormat from "react-number-format";
+import { Container, Col, Row } from "reactstrap";
 import "./app.css";
 
 import Loadable from "react-loadable";
 
-const covidEndPointAll = "https://coronavirus-19-api.herokuapp.com/all";
+const covidEndPointAll = "https://corona.lmao.ninja/all";
 
 const LoadableSumChart = Loadable({
   loader: () => import("./SummaryChart"),
@@ -29,39 +30,121 @@ function Summary() {
   }, []);
 
   return (
-    <div className="container summary-padding">
-      <h1 className="text-center">🦠 COVID19 Global</h1>
+    <div className="container-fluid summary-padding">
+      <h1 className="text-center">COVID19 Global</h1>
       <div className="sum-margin">
         <LoadableSumChart />
-        <div>
-          <ul className="numbers">
-            <li>
-              Cases:{" "}
-              <NumberFormat
-                value={data.cases}
-                thousandSeparator={true}
-                displayType={"text"}
-              />
-            </li>
-            <li>
-              Deaths:
-              <NumberFormat
-                value={data.deaths}
-                thousandSeparator={true}
-                displayType={"text"}
-              />
-            </li>
-            <li>
-              Recovered:
-              <NumberFormat
-                value={data.recovered}
-                thousandSeparator={true}
-                displayType={"text"}
-              />
-            </li>
-          </ul>
-        </div>
       </div>
+      <Row className="jumbotron">
+        <Col sm="4">
+          <Container>
+            <div class="card border-light mb-3">
+              <div class="card-header">Active Cases</div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+                <p class="card-text">
+                  {" "}
+                  <NumberFormat
+                    value={data.active}
+                    thousandSeparator={true}
+                    displayType={"text"}
+                  />
+                </p>
+              </div>
+            </div>
+          </Container>
+        </Col>
+        <Col xs="6" sm="4">
+          <Container>
+            <div class="card border-light mb-3">
+              <div class="card-header">Affected Countries</div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+                <p class="card-text">
+                  {" "}
+                  <NumberFormat
+                    value={data.affectedCountries}
+                    thousandSeparator={true}
+                    displayType={"text"}
+                  />
+                </p>
+              </div>
+            </div>
+          </Container>
+        </Col>
+        <Col xs="6" sm="4">
+          <Container>
+            <div class="card border-light mb-3">
+              <div class="card-header">People Healed</div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+                <p class="card-text">
+                  {" "}
+                  <NumberFormat
+                    value={data.recovered}
+                    thousandSeparator={true}
+                    displayType={"text"}
+                  />
+                </p>
+              </div>
+            </div>
+          </Container>
+        </Col>
+        <Col sm="4">
+          <Container>
+            <div class="card border-light mb-3">
+              <div class="card-header">Deaths</div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+                <p class="card-text">
+                  {" "}
+                  <NumberFormat
+                    value={data.deaths}
+                    thousandSeparator={true}
+                    displayType={"text"}
+                  />
+                </p>
+              </div>
+            </div>
+          </Container>
+        </Col>
+        <Col xs="6" sm="4">
+          <Container>
+            <div class="card border-light mb-3">
+              <div class="card-header">Today's Cases</div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+                <p class="card-text">
+                  {" "}
+                  <NumberFormat
+                    value={data.todayCases}
+                    thousandSeparator={true}
+                    displayType={"text"}
+                  />
+                </p>
+              </div>
+            </div>
+          </Container>
+        </Col>
+        <Col xs="6" sm="4">
+          <Container>
+            <div class="card border-light mb-3">
+              <div class="card-header">Today's Deaths</div>
+              <div class="card-body">
+                <h4 class="card-title"></h4>
+                <p class="card-text">
+                  {" "}
+                  <NumberFormat
+                    value={data.todayDeaths}
+                    thousandSeparator={true}
+                    displayType={"text"}
+                  />
+                </p>
+              </div>
+            </div>
+          </Container>
+        </Col>
+      </Row>
     </div>
   );
 }
