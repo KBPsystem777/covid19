39061a92bc32b2d0fe618de99c8fa4408e68d67d
kbpsystem <koleen.bp@outlook.com>
2020-04-06 13:33:31 +0800

    added info for each country

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Container, Row, Col } from "reactstrap";
+import NumberFormat from "react-number-format";
 
 const covidEndPoint = "https://corona.lmao.ninja/countries?sort=cases";
 
@@ -20,7 +21,9 @@ function PHsummary() {
   console.log(data);
   return (
     <div className="container summary-padding">
-      <h1 className="text-center">Countries Affected: {data.length}</h1>
+      <h1 className="text-center country-padding">
+        Countries Affected: {data.length + 1}
+      </h1>
       <Container className="countries-padding-top">
         {data.map((country, index) => {
           return (
@@ -28,10 +31,46 @@ function PHsummary() {
               <div className="jumbotron">
                 <Row>
                   <Col xs="6">
-                    <img className="flag" src={country.countryInfo.flag} />
+                    <img
+                      className="flag"
+                      src={country.countryInfo.flag}
+                      alt={`Flag of ${country.country}`}
+                    />
                   </Col>
                   <Col xs="6">
-                    <h4>{country.country}</h4>
+                    <h3>{country.country}</h3>
+                    <p>
+                      Cases:{" "}
+                      <NumberFormat
+                        value={country.cases}
+                        thousandSeparator={true}
+                        displayType={"text"}
+                      />
+                    </p>
+                    <p>
+                      Recovered:{" "}
+                      <NumberFormat
+                        value={country.recovered}
+                        thousandSeparator={true}
+                        displayType={"text"}
+                      />
+                    </p>
+                    <p>
+                      PUIs/PUMs:{" "}
+                      <NumberFormat
+                        value={country.active}
+                        thousandSeparator={true}
+                        displayType={"text"}
+                      />
+                    </p>
+                    <p>
+                      Deaths:{" "}
+                      <NumberFormat
+                        value={country.deaths}
+                        thousandSeparator={true}
+                        displayType={"text"}
+                      />
+                    </p>
                   </Col>
                 </Row>
               </div>
