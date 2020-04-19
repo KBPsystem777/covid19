3790fa571f4cef3f44a6111b9ca11175c50e13c4
kbpsystem <koleen.bp@outlook.com>
2020-04-19 14:24:38 +0800

    under construction page

---
+++ src/components/countries/CountryPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+const CountryPage = () => {
+  return (
+    <Container className="pages-padding-top">
+      <h1>Page under construction...</h1>
+    </Container>
+  );
+};
+
+export default CountryPage;
